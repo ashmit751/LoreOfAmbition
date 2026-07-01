a6a735f5309983315ca051f3b768803a9762b12a
Ashmit <ashmit.or.abhijeet@gmail.com>
2026-07-01 20:25:54 +0530

    named the tabs

--- Frontend/app/tabs/_layout.tsx
+++ Frontend/app/tabs/_layout.tsx
@@ -3,7 +3,11 @@ import "@/global.css"
 
 const TabLayout = () => (
   <Tabs screenOptions={{headerShown: false}} className="bg-background">
-    <Tabs.Screen name='index' options={{title: 'Home'}} />
+  <Tabs.Screen name='Home' options={{title: 'Home'}} />
+  <Tabs.Screen name='Discover' options={{title: 'Discover'}} />
+  <Tabs.Screen name='Create' options={{title: 'Create'}} />
+  <Tabs.Screen name='Profile' options={{title: 'Profile'}} />
+  <Tabs.Screen name='Challenges' options={{title: 'Challenges'}} />
   </Tabs>
 )
 export default TabLayout;
