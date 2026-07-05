94db40d6232d6e4c23d9025f8f1d4a665f576475
Ashmit <ashmit.or.abhijeet@gmail.com>
2026-07-06 00:08:52 +0530

    Create data.ts

---
+++ Frontend/constants/data.ts
@@ -0,0 +1,10 @@
+import {icons} from './icons';
+
+export const tabs = [
+    { name: 'index', icon: icons.home },
+    { name: 'discover', icon: icons.discover },
+    { name: 'create', icon: icons.create },
+    { name: 'challenges', icon: icons.challenges },
+    { name: 'profile', icon: icons.profile },
+
+];
