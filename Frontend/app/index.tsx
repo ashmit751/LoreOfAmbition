c9993942f9bc7f89ab04e85536b85e9452ac10c6
Ashmit <ashmit.or.abhijeet@gmail.com>
2026-07-21 12:46:14 +0530

    Update index.tsx

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,12 +1,4 @@
 import { Redirect } from 'expo-router';
-import { Image } from 'react-native';
-import { NAV_ICONS } from '@/constants/icons';
-
-<Image 
-  source={NAV_ICONS.home}
-  className="w-6 h-6"
-  resizeMode="contain"
-/>
 
 export default function Index() {
   return <Redirect href="/tabs" />;
